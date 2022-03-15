7aaffdd8b52cfe53b45f1e3d9e75f51634be6927
EnricoCavic <enricocavic@gmail.com>
2022-03-15 08:47:11 -0300

    Fixed box cast offset bug
    bug occured when changing rotation

--- Assets/ModularPackages/BoxCast/BoxCastCollision.cs
+++ Assets/ModularPackages/BoxCast/BoxCastCollision.cs
@@ -15,11 +15,10 @@ public class BoxCastCollision
     public bool toggleDraw;
 
     private const float OFFSET = 0.1f;
-    private Vector3 positionMultiplicator => -origin.InverseTransformDirection(direction) * OFFSET;
-    public Vector3 finalPosition => origin.position + positionMultiplicator;
     public Vector3 finalScale => Vector3.Scale(scaleModifier, origin.localScale);
     public Vector3 finalDirection => origin.TransformDirection(direction) * distanceToOrigin;
-
+    private Vector3 positionMultiplicator => -finalDirection * OFFSET;
+    public Vector3 finalPosition => origin.position + positionMultiplicator;
 
     public BoxCastCollision() {}
 
@@ -36,25 +35,6 @@ public class BoxCastCollision
             Gizmos.color = Color.green;
         else
             Gizmos.color = Color.red;
-
-        Vector3 center = finalPosition + finalDirection;
-        Vector3 maxPoint = center + Vector3.Scale(finalDirection, finalScale/2);
-        Vector3 pointR = maxPoint + Vector3.Scale(Vector3.Cross(finalDirection, origin.up), finalScale/2);
-        Vector3 rightUpV = pointR + Vector3.Scale(origin.up, finalScale/2);
-        /*
-        Vector3 point1 = finalPosition + finalDirection + finalScale/2;
-        Vector3 point2 = finalPosition + finalDirection - finalScale/2;
-        Vector3 point3 = point1 + origin.TransformVector(new Vector3(0,0,-1));
-        Vector3 point4 = finalPosition + finalDirection;
-        */
-
-        Gizmos.DrawLine(origin.position, center);
-        Gizmos.DrawLine(center, maxPoint);
-        Gizmos.DrawLine(maxPoint, pointR);
-        Gizmos.DrawLine(pointR, rightUpV);
-        //Gizmos.DrawLine(origin.position, point3);
-        //Gizmos.DrawLine(origin.position, point4);
-        
         
         Gizmos.DrawWireCube(finalPosition + finalDirection, finalScale);
 
